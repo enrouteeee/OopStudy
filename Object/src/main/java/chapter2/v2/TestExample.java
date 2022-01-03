375509136e750555ae3f24d98e92979968b383ba
enrouteeee <enrouteeee@gmail.com>
2022-01-03 17:41:31 +0900

    chapter2. 객체지향 프로그래밍

---
+++ Object/src/main/java/chapter2/v2/TestExample.java
@@ -0,0 +1,18 @@
+package chapter2.v2;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class TestExample {
+    public static void main(String[] args) {
+        Movie avatar = new Movie("아바타",
+                Duration.ofMinutes(120),
+                Money.wons(10000),
+                new AmountDiscountPolicy(Money.wons(800),
+                        new SequenceCondition(1),
+                        new SequenceCondition(10),
+                        new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59))));
+
+    }
+}
